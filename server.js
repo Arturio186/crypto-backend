e9c0bf2a171d75819b59269f63fbfa461727197c
Arturio186 <bain.romanoff@mail.ru>
2025-05-03 18:43:29 +0500

    app

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const cron = require('node-cron');
 const cors = require('cors');
-const https = require('https');
-const fs = require('fs');
 
 const fetchRates = require('./services/rates/fetchRates');
 const getUsdtRate = require('./services/rates/getUsdtRate');
@@ -11,11 +9,6 @@ const PORT = process.env.PORT || 3000;
 const environment = process.env.NODE_ENV || 'development';
 
 const app = express();
-
-const options = {
-  key: fs.readFileSync('/etc/ssl/private/nginx-selfsigned.key'),
-  cert: fs.readFileSync('/etc/ssl/certs/nginx-selfsigned.crt')
-};
 
 const allowedOrigins = [
   'https://arturio186-crypto-frontend-02c6.twc1.net',
@@ -39,14 +32,9 @@ app.use(express.json());
 
 app.get('/getUsdtRate', getUsdtRate);
 
-/*
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`Environment: ${environment}`);
-}); */
-
-https.createServer(options, app).listen(3000, '0.0.0.0', () => {
-  console.log('HTTPS Server running on port 3000');
-});
+}); 
 
 cron.schedule('* * * * *', fetchRates);
